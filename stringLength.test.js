ab3a24419c871c7ef384870ff626c0cff5664f21
abreham1222 <abreham1222@gmail.com>
2022-11-11 22:39:29 +0300

    Add testing string length

--- stringLength.test.js
+++ stringLength.test.js
@@ -3,3 +3,20 @@ describe('stringLength', ()=>{
   test('check for stringLength exception', ()=> {
     // Arrange
     const string= "Hello, world!";
+
+      expect(()=>stringLength(string)).toThrow();
+      expect(()=>stringLength(string)).toThrow(Error);
+  });
+
+  test('give a string value: "Hello" and check what is his length', () => {
+  // Arrange
+    const string= "Hello";
+
+    // Act
+    const result = stringLength(string);
+
+    // Assert
+  expect(result).toBe(5);
+   });
+
+})
