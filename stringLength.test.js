6e25c033e10c348e1dd4002f511f5d7e36b1d3fc
abreham1222 <abreham1222@gmail.com>
2022-11-11 22:38:35 +0300

    Add string legth test

---
+++ stringLength.test.js
@@ -0,0 +1,5 @@
+const stringLength = require('./stringLength');
+describe('stringLength', ()=>{
+  test('check for stringLength exception', ()=> {
+    // Arrange
+    const string= "Hello, world!";
