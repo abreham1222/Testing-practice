2bf2cb043b817b5667a85e1df5f815b4b0e12ed3
abreham1222 <abreham1222@gmail.com>
2022-11-11 22:43:03 +0300

    Add testing

--- calculator.test.js
+++ calculator.test.js
@@ -32,3 +32,110 @@ describe('new calculator add method', ()=>{
   
 });
 
+describe('new calculator substract method', ()=>{
+  test('substracts 2 - 1  to equal 1', ()=>{
+    //Arrange
+    const calculate= new calculator(2,1);
+
+    //Act
+    const result = calculate.substract();
+
+    //Assert
+    expect(result).toBe(1);
+  });
+
+  test('substracts 1 - 2  to equal -1', ()=>{
+    //Arrange
+    const calculate= new calculator(1,2);
+
+    //Act
+    const result = calculate.substract();
+
+    //Assert
+    expect(result).toBe(-1);
+  });
+
+  test('substracts 2.5 - 1  to equal 1.5', ()=>{
+    //Arrange
+    const calculate= new calculator(2.5,1);
+
+    //Act
+    const result = calculate.substract();
+
+    //Assert
+    expect(result).toBeCloseTo(1.5);
+  });
+
+});
+
+describe('new calculator divide method', ()=>{
+  test('divids 1 / 2  to equal 0.5', ()=>{
+    //Arrange
+    const calculate= new calculator(1,2);
+
+    //Act
+    const result = calculate.divide();
+
+    //Assert
+    expect(result).toBeCloseTo(0.5);
+  });
+
+  test('divids 0 / 1  to equal 0', ()=>{
+    //Arrange
+    const calculate= new calculator(0);
+
+    //Act
+    const result = calculate.divide();
+
+    //Assert
+    expect(result).toBe(0);
+  });
+
+  test('divids -1 / -4  to equal 0.25', ()=>{
+    //Arrange
+    const calculate= new calculator(-1,-4);
+
+    //Act
+    const result = calculate.divide();
+
+    //Assert
+    expect(result).toBeCloseTo(0.25);
+  });
+
+});
+
+describe('new calculator multiply method', ()=>{
+  test('multiplies 0 * 2  to equal 0', ()=>{
+    //Arrange
+    const calculate= new calculator(0,2);
+
+    //Act
+    const result = calculate.multiply();
+
+    //Assert
+    expect(result).toBe(0);
+  });
+
+
+  test('multiplies -1 * 2  to equal -2', ()=>{
+    //Arrange
+    const calculate= new calculator(-1,2);
+
+    //Act
+    const result = calculate.multiply();
+
+    //Assert
+    expect(result).toBe(-2);
+  });
+
+  test('multiplies -4 * -2  to equal 8', ()=>{
+    //Arrange
+    const calculate= new calculator(-4,-2);
+
+    //Act
+    const result = calculate.multiply();
+
+    //Assert
+    expect(result).toBe(8);
+  });
+});
