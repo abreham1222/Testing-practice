d0b52388cb8f51ccf406ff6c4e32b0e17351e850
abreham1222 <abreham1222@gmail.com>
2022-11-11 22:37:42 +0300

    Add string legth

--- stringLength.js
+++ stringLength.js
@@ -3,3 +3,9 @@ const length = string.split('').length;
 if(length <1 || length >10){
 throw new Error('The string length does not meet the requirements.');
 }
+else {
+return length;
+}
+}
+
+module.exports = stringLength;
