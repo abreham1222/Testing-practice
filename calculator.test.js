a6bfa5fa489bf12dc3c698ce8d1bf53fadf09cf5
abreham1222 <abreham1222@gmail.com>
2022-11-11 22:41:49 +0300

    Add testing of class calculator

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,8 @@ describe('new calculator add method', ()=>{
   test('adds 1 + 2  to equal 3', ()=>{
     //Arrange
     const calculate= new calculator(1,2);
+      const result = calculate.add(1, 2);
+
+    //Assert
+    expect(result).toBe(3);
+  });
